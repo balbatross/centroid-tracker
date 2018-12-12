a97d680169d2fd5ed2e55215a4bccf6d8543451e
Ross Leitch <ross@end-game.com>
2018-12-12 16:00:27 +1300

    Init

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+        name="centroid_tracker",
+        version="0.0.1",
+        author="Ross Leitch",
+        author_email="ross@end-game.com",
+        description="A small centroid tracker library based off https://www.pyimagesearch.com/2018/07/23/simple-object-tracking-with-opencv/",
+        url="https://github.com/balbatross/centroid-tracker",
+        packages=['centroid-tracker'],
+        install_requires=[
+            'scipy',
+            'numpy'
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+        ],
+)
